b70dc1079922c14057e61b120559b356b891c97f
Alysha Thistlethwaite <alysha.thistlethwaite@gmail.com>
2019-03-05 15:19:08 -0800

    Changes in scss page-templates

--- themes/inhabitent-theme/page.php
+++ themes/inhabitent-theme/page.php
@@ -19,8 +19,17 @@ get_header(); ?>
 
 			<?php endwhile; // End of the loop. ?>
 
-
 		</main><!-- #main -->
+
+	<form>
+		<div class="info">
+			 <p class="name"><label>Name <span class="required">*</span></label><span class="your-name"><input type="text" name="your-name" value="" size="40"></span> </p>
+			 <p class="email"><label>Email <span class="required">*</span></label><span class="your-email"><input type="email" name="your-email" value="" size="40"></span> </p>
+			 <p class="subject"><label>Subject <span class="required">*</span></label><span class="your-subject"><input type="text" name="your-subject" value="" size="40"></span> </p>
+			 <p class="message"><label>Message <span class="required">*</span></label><span class="your-message"><textarea name="your-message" cols="30" rows="10"></textarea></span> </p>
+			 <input type="submit" value="Submit" class="submit">
+		</div>
+	</form>
 
 	</div><!-- #primary -->
 	<?php get_sidebar(); ?>	
--- themes/inhabitent-theme/template-parts/content-page.php
+++ themes/inhabitent-theme/template-parts/content-page.php
@@ -23,13 +23,4 @@
 		</div>
 	</div><!-- .entry-content -->
 
-	<form>
-		<div class="info">
-			 <p class="name"><label>Name <span class="required">*</span></label><span class="your-name"><input type="text" name="your-name" value="" size="40"></span> </p>
-			 <p class="email"><label>Email <span class="required">*</span></label><span class="your-email"><input type="email" name="your-email" value="" size="40"></span> </p>
-			 <p class="subject"><label>Subject <span class="required">*</span></label><span class="your-subject"><input type="text" name="your-subject" value="" size="40"></span> </p>
-			 <p class="message"><label>Message <span class="required">*</span></label><span class="your-message"><textarea name="your-message" cols="30" rows="10"></textarea></span> </p>
-			 <input type="submit" value="Submit" class="submit">
-</div>
-</form>
 </article><!-- #post-## -->
